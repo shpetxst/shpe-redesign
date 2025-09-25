c8713ac3c9e2321c8f2e0aaec0f2b8e4dc644992
Thomas Mendoza <thomasadeelmendoza@gmail.com>
2025-09-24 22:20:26 -0500

    updated dependencies and added Vercel Analytics integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
@@ -53,6 +54,7 @@ function App() {
     <Router>
       <ScrollToTop />
       <AppContent />
+      <Analytics />
     </Router>
   );
 }
